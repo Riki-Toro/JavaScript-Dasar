7e867f5e7ae33efc57774617d5a152c4a946acba
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-01-26 14:57:09 +0700

    Create rekursif.js

---
+++ rekursif.js
@@ -0,0 +1,40 @@
+// REKURSIF
+	function tes() {
+	return tes();
+	}
+	tes()
+
+
+// pengulangan 10 sampai 1
+	for (var i = 10; i >= 1; i--) {
+		console.log(i);
+	}
+
+
+// rekursif tampilAngka
+// BASE CASE adalah kondisi akhir dari rekursif yang menghasulkan nilai
+	function tampilAngka(n) {
+		if (n === 0) {
+			return;
+		}
+		console.log(n);
+		return tampilAngka(n-1);
+	}
+
+	tampilAngka(10);
+
+
+
+// FAKTORIAL
+	function faktorial(n) {
+		if (n === 0) return 1;
+		return n * faktorial(n-1);
+	}
+
+
+// Implementasi Rekursif
+	// 1. Menggantikan looping
+	// Fibonacci
+	// Pencarian dan penelusuran pada struktur data list dan tree
+	// Bahasa pemrograman yang tidak memiliki pengulangan (Haskel, Erlang, Prolog, dll)
+	// Dll
